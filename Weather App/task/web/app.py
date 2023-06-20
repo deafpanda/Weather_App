be6236a1a3938a95bdcd29a51213b2fd0455c828
megan <magraye@protonmail.com>
2023-06-19 20:33:54 -0700

    Stage 1 - passed

--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -3,10 +3,28 @@ import sys
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return 'Hello, world!'
+    # return 'Hi! This is the response from the Flask application'
+
+
+@app.route('/profile')
+def profile():
+    return 'This is profile page'
+
+
+@app.route('/login')
+def log_in():
+    return 'This is login page'
+
+
 # don't change the following way to run flask:
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         arg_host, arg_port = sys.argv[1].split(':')
         app.run(host=arg_host, port=arg_port)
     else:
-        app.run()
+        app.run(debug=True)
+
